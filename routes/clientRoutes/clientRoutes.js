b88a347b9ba25562422396d0578bde36ab8f78ba
Ashok K <kashok2001cec@gmail.com>
2026-04-30 18:32:39 +0530

    new changes

--- routes/clientRoutes/clientRoutes.js
+++ routes/clientRoutes/clientRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ClientController=require("../../controller/client/clientController")
 
 router.get("/send-services-client",ClientController.getAllTheServicesSentToClient)
+router.get("/send-services-client/:id",ClientController.getAllTheServicesSentToClientId)
 
 
 module.exports=router
